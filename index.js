9df71cec61e98744513b05ec2ca26f2cd1e32d51
Priya Kumari <priyakumariyt05@gmail.com>
2024-03-06 17:18:59 +0530

    upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let Menubtn = document.getElementById("Menubtn");
+Menubtn.addEventListener("click", function (e) {
+  document.querySelector("body").classList.toggle("mobile-nav-active");
+  this.classList.toggle("fa-times");
+});
